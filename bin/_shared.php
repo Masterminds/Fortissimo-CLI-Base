353819657d2a75b2282cde4636057634cd9ab5e9
Matt Farina <matt@mattfarina.com>
2012-12-12 10:36:08 -0500

    Updating to work with symfony 2.1.x

--- bin/_shared.php
+++ bin/_shared.php
@@ -40,8 +40,9 @@ $registry->logger('\Fortissimo\Logger\OutputInjectionLogger', 'foil');
 $iterator = Finder::create()->files()->name('*.php')->in(array(FORT_APP_PATH . '/config'));
 
 // Because this iterator may be used in a Phar file we want to specify the PhpAdapter.
-$iterator->removeAdapters();
-$iterator->addAdapter(new PhpAdapter());
+// @todo When moving to symfony version greater than 2.1.x uncomment the following 2 lines.
+//$iterator->removeAdapters();
+//$iterator->addAdapter(new PhpAdapter());
 $config = iterator_to_array($iterator);
 foreach ($config as $file) {
   require_once $file;
